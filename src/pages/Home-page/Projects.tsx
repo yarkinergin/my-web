7e0d4590dec2fa690648b9a04b12943b181e64f9
Yarkın Ergin <yarkin.ergin@ug.bilkent.edu.tr>
2025-05-30 19:47:28 +0100

    nc

--- src/pages/Home-page/Projects.tsx
+++ src/pages/Home-page/Projects.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Col, Container, Row } from 'react-bootstrap';
 
 import dcLogo from "../../images/dc-logo.png";
+import ncLogo from "../../images/nc.png";
 
 
 function Projects () {
@@ -20,6 +21,21 @@ function Projects () {
                                 <div className="mask" style={{backgroundColor: "hsla(0, 0%,98%, 0.2)"}}>
                                     <div className="d-flex justify-content-center align-items-end pb-3 h-100">
                                         <p className="text-white mb-0">Discord Bot</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                        </div>
+                    </Col>
+                    <Col xs lg={4} className="work-img-col py-4">
+                        <div className="bg-image ripple rounded" data-mdb-ripple-color="light">
+                        <a href="https://next.yarkinergin.com">
+                        <img src={ncLogo} alt="dcLogo" className="w-50" />
+                            <div className="mask" style={{backgroundColor: "hsla(0, 0%, 0%, 0.4)"}}>
+                            </div>
+                            <div className="hover-overlay">
+                                <div className="mask" style={{backgroundColor: "hsla(0, 0%,98%, 0.2)"}}>
+                                    <div className="d-flex justify-content-center align-items-end pb-3 h-100">
                                     </div>
                                 </div>
                             </div>
